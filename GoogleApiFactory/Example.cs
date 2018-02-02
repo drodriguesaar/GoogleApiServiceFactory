f168625b98bf388260a7ee53ae2ebafb9ca79d12
Daniel Rodrigues de Almeida Araújo <drodriguesaar@gmail.com>
2018-02-02 16:03:32 -0200

    Add project files.

---
+++ GoogleApiFactory/Example.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Admin.Directory.directory_v1;
+using Google.Apis.Calendar.v3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleApiFactory
+{
+    public class Example
+    {
+        void Call()
+        {
+            var serviceAcccount = new GoogleApiServiceAccountFactory
+            {
+                GoogleServiceAccountAdminUser = "services.admin@domain.xx",
+                GoogleServiceAccountEmail = "user@g-proj.iam.gserviceaccount.com",
+                GoogleServiceAccountFilePath = "X:\file.json",
+                GoogleServiceApplicationName = "G-Api Factory"
+            };
+
+            /*Accessing proprietary domain data with service account admin user authentication.*/
+            var directoryService = serviceAcccount.GetService<DirectoryService>(new string[] { DirectoryService.Scope.AdminDirectoryUser });
+            var usersListRequest = directoryService.Users.List();
+            var usersListResult = usersListRequest.Execute();
+
+            /*
+             DO YOUR MAGIC
+             */
+             
+            /* Accessing data from a specific user from our domain, 
+             * use the userToImpersonate overload and the service account will authenticate the user on his behalf.*/
+            var calendarService = serviceAcccount.GetService<CalendarService>(new string[] { CalendarService.Scope.Calendar }, "user.me@domain.xx");
+            var calendarListRequest = calendarService.CalendarList.List();
+            var calendarListResult = calendarListRequest.Execute();
+
+            /*
+             DO YOUR MAGIC
+             */
+        }
+    }
+}
---
+++ GoogleApiServiceFactory.Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Google.Apis.Admin.Directory.directory_v1;
+
+namespace GoogleApiServiceFactory.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectDirectoryService();
+        }
+
+        static void ConnectDirectoryService() {
+            var gApiServiceFactory = new GoogleApiServiceAccountFactory
+            {
+                GoogleServiceAccountAdminUser = "service.admin@domain.br",
+                GoogleServiceAccountEmail = "serviceuser@g-proj.iam.gserviceaccount.xx",
+                GoogleServiceAccountFilePath = @"X:\\cert.json",
+                GoogleServiceApplicationName = "App G-Service",
+            };
+
+            var directoryService = gApiServiceFactory.GetService<DirectoryService>(new string[] { DirectoryService.Scope.AdminDirectoryUser });
+            var usersListRequest = directoryService.Users.Get("your.user@domain.xx");
+            var usersListResult = usersListRequest.Execute();
+
+            gApiServiceFactory.IsJSON = false;
+            gApiServiceFactory.GoogleServiceAccountFilePath = @"\\cert.p12";
+
+            var directoryServiceP12 = gApiServiceFactory.GetService<DirectoryService>(new string[] { DirectoryService.Scope.AdminDirectoryUser });
+            var usersListRequestP12 = directoryService.Users.Get("your.user@domain.xx");
+            var usersListResultP12 = usersListRequest.Execute();
+        }
+    }
+}
